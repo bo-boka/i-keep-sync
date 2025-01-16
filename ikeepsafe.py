1b787fbb96ba88883df85fbf42b139747a523bd9
Bo Boka <bosleyboka@gmail.com>
2025-01-15 22:23:52 -0800

    Subproducts, create CSV, Results Summary

--- ikeepsafe.py
+++ ikeepsafe.py
@@ -6,8 +6,9 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import traceback
+import re
 
-# List of target certification alt properties
+# Mappings of target certification alt properties to preferred sheet names
 CERTIFICATIONS = {
     "FERPA Certified": "FERPA",
     "COPPA Safe Harbor": "COPPA",
@@ -16,9 +17,15 @@ CERTIFICATIONS = {
 }
 
 
-def scrape_page(driver, product_data):
+def scrape_page(driver, product_data, prods_w_subs):
     """
-    Scrapes individual pages, looping through each product div to pull data & update Dataframe
+    Scrapes individual pages, looping through each product div and subproducts to pull data & update Dataframe.
+    Attributes scraped:
+        -Product Name
+        -Company Name
+        -Website
+        -Certifications
+        -Sub Products
     :param driver: Chrome driver with iKeepSafe website endpoint with product list
     :param product_data: Dict of product data pulled from website
     """
@@ -26,48 +33,106 @@ def scrape_page(driver, product_data):
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
 
-    # All products listed in product__gric div
+    # All products listed in product__grid div
     products_grid = soup.find('div', class_='products__grid')
-
     if not products_grid:
         raise Exception("No products found on the page.")
 
-    prod_count = 0
+    prod_count = 0  # For logging purposes
+
     for product in products_grid.find_all('div', recursive=False):
 
-        h4_span = product.find('span', class_='h4')  # Product name in span class h4
-        h5_class = product.find('div', class_='h5')  # Product name in div class h5
-        product_name = h4_span.get_text(strip=True) if h4_span else None
-        product_company = h5_class.get_text(strip=True) if h5_class else None
+        name_h4_span = product.find('span', class_='h4')  # Product name in span class h4
+        comp_h5_div = product.find('div', class_='h5')  # Product name in div class h5
+        product_name = name_h4_span.get_text(strip=True) if name_h4_span else None
+        product_company = comp_h5_div.get_text(strip=True) if comp_h5_div else None
 
-        if product_name or product_company:
+        if product_company and not product_name:
+            raise Exception("Missing Product Name for Company: "+product_company)
 
-            # Extract "View Website" link
-            website_link = None
-            view_website_element = product.find('a', string="View Website")
-            if view_website_element and view_website_element.get('href'):
-                website_link = view_website_element['href']
+        if product_name:
 
-            # Initialize certification flags
+            # Extract product ID from class tag attribute, which is only in parent products
+            product_classes = product.get("class", [])
+            product_id = None  # Default to None if no ID found
+            # Search for a class matching the pattern "product--<digits>"
+            for cls in product_classes:
+                match = re.match(r"product--(\d+)", cls)
+                if match:
+                    product_id = int(match.group(1))  # Extract the numeric ID
+                    break
+
+            # Initialize certification columns
             cert_flags = {cert: False for cert in CERTIFICATIONS.values()}
 
-            # Find certifications in <div class="product__certs">
-            cert_div = product.find('div', class_='product__certs')
-
+            cert_div = product.find('div', class_='product__certs')  # Certs in <div class="product__certs">
             if not cert_div:
-                raise Exception("Product listed with no certifications.")
+                raise Exception("Product listed with no certifications: "+product_name)
+            for cert_img in cert_div.find_all('img'):  # Certs listed as images; text is in alt attribute
+                cert_alt = cert_img.get('alt', '').strip()
+                if cert_alt in CERTIFICATIONS:
+                    cert_flags[CERTIFICATIONS[cert_alt]] = True
+                else:
+                    raise Exception(f"Unknown cert, {cert_alt}, for {product_name}.")
 
-            if cert_div:
-                for cert_img in cert_div.find_all('img'):
-                    cert_alt = cert_img.get('alt', '').strip()
-                    if cert_alt in CERTIFICATIONS:
-                        cert_flags[CERTIFICATIONS[cert_alt]] = True
+            # Parent product website is listed in different divs depending on the existence of sub-products.
+            # If sub-products exist, parent product website is in product__companyFooter. Else, in product__footer
+            main_product_link = None
+            # Check if sub-products exist
+            subproducts_div = product.find('div', class_='product__subproducts')
+            if subproducts_div:
+                prods_w_subs.append(product_name)  # For logging purposes
+
+                # Extract main product "View Website" link from div specific to sub-products
+                company_footer = product.find('div', class_='product__companyFooter')
+                if company_footer:
+                    link_element = company_footer.find('a', string="View Website")
+                    if link_element and link_element.get('href'):
+                        main_product_link = link_element['href']
+                else:
+                    raise Exception("Existing sub-products without product__companyFooter div: "+product_name)
+
+                # Loop through sub-products, which are in list tags
+                for subproduct in subproducts_div.find_all('li'):
+                    subproduct_name_div = subproduct.find('div')  # Sub-product names in naked div tag within list
+                    subproduct_name = subproduct_name_div.get_text(strip=True) if subproduct_name_div else None
+
+                    if not subproduct_name:
+                        raise Exception("Missing sub-product name in list for main product: "+product_name)
+
+                    subproduct_link = main_product_link  # Default to parent link, if no sub link found.
+                    # Check for sub-product "View Website" link
+                    # Sub link View Website text has additional arrow icons. Use contains logic rather than exact match.
+                    sub_link_element = subproduct.find('a')
+                    if sub_link_element and "View Website" in sub_link_element.text:
+                        subproduct_link = sub_link_element['href']
+
+                    # Add sub-product data
+                    product_data.append({
+                        "IKS ID": product_id,
+                        "Product Name": subproduct_name,
+                        "Product Company": product_company,
+                        "Website": subproduct_link,
+                        **cert_flags
+                    })
+
+                    prod_count += 1
+            else:
+                # Extract main product "View Website" link
+                footer = product.find('div', class_='product__footer')
+                if footer:
+                    link_element = footer.find('a', string="View Website")
+                    if link_element and link_element.get('href'):
+                        main_product_link = link_element['href']
+                else:
+                    raise Exception("No subproducts with no product__footer div: "+product_name)
 
             # Add data to the DataFrame
             product_data.append({
+                "IKS ID": product_id,
                 "Product Name": product_name,
                 "Product Company": product_company,
-                "Website": website_link,
+                "Website": main_product_link,
                 **cert_flags
             })
 
@@ -78,22 +143,23 @@ def scrape_page(driver, product_data):
 
 def main():
     """
-    Scrapes iKeepSafe for product certifications, spanning multiple pages.
-    TODO: Realized there are also subproducts on some of the products (see GoGuardian & ExploreLearning) that need to also be looped through
+    Scrapes data, page by page, converts to CSV.
     :return:
     """
-    # set up Chrome and Selenium
-    driver = webdriver.Chrome()
-    driver.get('https://ikeepsafe.org/products/#all')
 
-    product_data = []
-    last_page_number = None
-    prod_total = 0
+    try:
+        # set up Chrome and Selenium
+        driver = webdriver.Chrome()
+        driver.get('https://ikeepsafe.org/products/#all')
 
-    while True:
-        try:
+        product_data = []
+        prods_w_subs = []
+        last_page_number = None
+        prod_total = 0
+
+        while True:
             # scrape page, return product count to add to total count
-            prod_total += scrape_page(driver, product_data)
+            prod_total += scrape_page(driver, product_data, prods_w_subs)
 
             current_page_number = WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "span.products__pagination__item--active"))
@@ -123,19 +189,32 @@ def main():
                 break
 
             next_button.click()
-            time.sleep(2)  # Wait for the page to load; adjust timing as needed
-        except Exception as e:
-            print(traceback.format_exc())
-            break
+            time.sleep(1)  # Wait for the page to load; adjust timing as needed
 
-    driver.quit()
-    print("Product Count: ", prod_total)
+        driver.quit()
 
-    # Convert the product data to a Pandas DataFrame
-    df = pd.DataFrame(product_data)
-    print(df)
-    print("Dataframe Row Count", len(df))
+        df = pd.DataFrame(product_data)
+        df.to_csv("iKeepSafe_cert_data_01_2025.csv", index=False)
 
+        # Results
+        print(df.head(10))
+        print("Dataframe Row Count", len(df))
+        print("Product Count: ", prod_total)
+        print("Products containing sub-products", prods_w_subs)
+        # Count products by certification
+        certification_summary = df[["FERPA", "COPPA", "CSPC", "ATLIS"]].sum()
+        certification_summary["Total"] = certification_summary.sum()
+        print(certification_summary)
+
+    except Exception as e:
+        driver.quit()
+        df = pd.DataFrame(product_data)
+        # Results
+        print(df.head(10))
+        print("Dataframe Row Count", len(df))
+        print("Product Count: ", prod_total)
+        print("Products containing sub-products", prods_w_subs)
+        print(traceback.format_exc())
 
 # Run the function
 main()
